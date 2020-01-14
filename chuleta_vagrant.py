c57869d9767c196a61cc3b888cb1c555e056970a
shalooooo <gonzalopvargas@gmail.com>
2020-01-14 12:56:07 -0300

    Update chuleta_vagrant.py
    se agrega ejemplo de VM con ssh

--- chuleta_vagrant.py
+++ chuleta_vagrant.py
@@ -15,3 +15,15 @@
     yum groupinstall 'KDE' 'X Window System'
     systemctl set-default graphical.target
     reboot
+    
+    # Ejemplo maquina virtual
+    """
+    # -*- mode: ruby -*-
+    # vi: set ft=ruby :
+    Vagrant.configure("2") do |config|
+        config.vm.box = "ubuntu/trusty64"
+        config.vm.network "private_network", ip:"192.168.40.40"           
+        config.vm.provision "shell", privileged: false,
+          inline: "curl https://github.com/danirod.keys >> ~/.ssh/authorized_keys"
+    end
+    """
